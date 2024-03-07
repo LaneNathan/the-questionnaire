14a03ec4a1fc99f555d7b0ff64e041a621a29fc2
Nathan Lane <lanenathan214@gmail.com>
2024-03-07 14:26:47 -0500

    created questions for quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [ 
+    {
+        questions:'What is an Array?',
+        choices:[' a collection of elements of the same data type','a guy named Ray','a type of stingray','a web program'],
+        answer:'a collection of elements of the same data type',
+    },
+    {
+        questions:'Which of the following is not a javascript data type?',
+        choices:['Null type','Undefined type',' Number type','All of the mentioned'],
+        answer:'All of the mentioned',
+    },
+    {
+        questions:'Which of the following can be used to call a JavaScript Code Snippet?',
+        choices:[' Function/Method','Preprocessor','Triggering Event','css'],
+        answer:'Function/Method',
+    },
+    {
+        questions:'What is HTML?',
+        choices:['HTML describes the structure of a webpage',' HTML is the standard markup language mainly used to create web pages','HTML consists of a set of elements that helps the browser how to view the content',' All of the mentioned'],
+        answer:' All of the mentioned',
+    },
+    {
+        questions:'HTML stands for __________',
+        choices:['HyperText Markup Language','HyperText Machine Language','HyperText Marking Language','HighText Marking Language'],
+        answer:'HyperText Markup Language',
+    },
+];
