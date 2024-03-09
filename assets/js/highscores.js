4d6d96f9531b9b25206720d03d1917f73ab2f3f6
Nathan Lane <lanenathan214@gmail.com>
2024-03-08 20:17:22 -0500

    created functions for getting highscores and clearing them out

---
+++ assets/js/highscores.js
@@ -0,0 +1,26 @@
+function getHighScores(){
+    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+    highscores.sort(function(a,b){
+        return b.score - a.score;
+    });
+
+    for(var i = 0; i<highscores.length; i += 1){
+
+        var list = document.createElement('li');
+         list.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+
+        var orderList = document.getElementById('highscores');
+        orderList.appendChild(list);
+
+    }
+}
+
+function clearHighscore(){
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+}
+
+document.getElementById('clear').onclick = clearHighscore;
+
+printHighscores();
